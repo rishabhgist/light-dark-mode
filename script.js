490efd92c303868a2fdb58cba0311808ea0f2726
Rishabh Gist <damyrachit@gmail.com>
2022-10-17 13:36:14 +0530

    fixed issue with saving themes

--- script.js
+++ script.js
@@ -35,9 +35,11 @@ function lightMode() {
 function switchTheme(event) {
     if (event.target.checked) {
         document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
         darkMode();
     } else {
         document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
         lightMode();
     }
 }
@@ -45,3 +47,14 @@ function switchTheme(event) {
 
 // Event Listner for Toggle Button
 toggleSwitch.addEventListener('change', switchTheme);
+
+// Check localStorage for current Theme
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+        darkMode();
+    }
+}
