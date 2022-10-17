129c81a938317709621b86b1de14391f6797788c
Rishabh Gist <damyrachit@gmail.com>
2022-10-17 13:29:50 +0530

    added imgMode method

--- script.js
+++ script.js
@@ -6,16 +6,20 @@ const image2 = document.getElementById('image2');
 const image3 = document.getElementById('image3');
 const textBox = document.getElementById('text-box');
 
+// Dark or Light Images
+function imgMode(color) {
+    image1.src = `img/undraw_proud_coder_${color}.svg`;
+    image2.src = `img/undraw_feeling_proud_${color}.svg`;
+    image3.src = `img/undraw_conceptual_idea_${color}.svg`;
+}
+
 // Dark Mode 
 function darkMode() {
     nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
     textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)';
     toggleIcon.children[0].textContent = 'Dark Mode';
-    toggleIcon.children[1].classList.remove('fa-sun');
-    toggleIcon.children[1].classList.add('fa-moon');
-    image1.src = 'img/undraw_proud_coder_dark.svg';
-    image2.src = 'img/undraw_feeling_proud_dark.svg';
-    image3.src = 'img/undraw_conceptual_idea_dark.svg';
+    toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon');
+    imgMode('dark');
 }
 
 // Light Mode
@@ -23,11 +27,8 @@ function lightMode() {
     nav.style.backgroundColor = 'rgb(255 255 255 / 50%)';
     textBox.style.backgroundColor = 'rgb(0 0 0 / 50%)';
     toggleIcon.children[0].textContent = 'Light Mode';
-    toggleIcon.children[1].classList.remove('fa-moon');
-    toggleIcon.children[1].classList.add('fa-sun');
-    image1.src = 'img/undraw_proud_coder_light.svg';
-    image2.src = 'img/undraw_feeling_proud_light.svg';
-    image3.src = 'img/undraw_conceptual_idea_light.svg';
+    toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
+    imgMode('light');
 }
 
 // This function will switch the theme
